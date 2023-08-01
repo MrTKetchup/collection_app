f98e58bb118debe8708850544c867c6d1be0e5e8
MrTKetchup <alexanderjamesmansfield@gmail.com>
2023-08-01 13:59:03 +0100

    initial RecordTest.php skeleton created

---
+++ tests/RecordTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once('src/Record.php');
+
+use PHPUnit\Framework\TestCase;
+
+class RecordTest extends TestCase {
+
+    public function test_create_record_card_success() 
+    {
+        
+    }
+}
